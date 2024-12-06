2a46d78c037a656b3aca898f7f0cbe988f0834fe
Remi <remi.jospin@gmail.com>
2024-12-06 15:06:28 +0100

    demo command

---
+++ design_pattern/src/main/java/org/example/behavioral/demos/command/Command.java
@@ -0,0 +1,5 @@
+package org.example.behavioral.demos.command;
+
+public interface Command {
+    void execute();
+}
---
+++ design_pattern/src/main/java/org/example/behavioral/demos/command/Light.java
@@ -0,0 +1,11 @@
+package org.example.behavioral.demos.command;
+
+public class Light {
+    public void turnOn(){
+        System.out.println("Light on");
+    }
+
+    public void turnOff(){
+        System.out.println("Light off");
+    }
+}
