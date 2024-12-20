f13bfe95ef6bbeeaa450f0e0474931537f2a2976
JOSPINRemi <remidequarouble@gmail.com>
2024-12-20 11:15:45 +0100

    maj exercice 22

--- exercicesThread/src/main/java/org/example/exercice22/Main.java
+++ exercicesThread/src/main/java/org/example/exercice22/Main.java
@@ -5,17 +5,21 @@ import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
-        CompletableFuture<Void> read = CompletableFuture.runAsync(() -> {
-            System.out.println("1. Lecture des données...");
-        }, Executors.newVirtualThreadPerTaskExecutor());
+        try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            CompletableFuture<Void> read = CompletableFuture.runAsync(() -> {
+                System.out.println("1. Lecture des données...");
+            }, executor);
 
-        CompletableFuture<Void> process = read.thenRunAsync(() -> {
-            System.out.println("2. Traitement des données...");
-        }, Executors.newVirtualThreadPerTaskExecutor());
+            CompletableFuture<Void> process = read.thenRunAsync(() -> {
+                System.out.println("2. Traitement des données...");
+            }, executor);
 
-        CompletableFuture<Void> store = process.thenRunAsync(() -> {
-            System.out.println("3. Stockage des résultats...");
-        }, Executors.newVirtualThreadPerTaskExecutor());
-        store.join();
+            CompletableFuture<Void> store = process.thenRunAsync(() -> {
+                System.out.println("3. Stockage des résultats...");
+            }, executor);
+            store.join();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
