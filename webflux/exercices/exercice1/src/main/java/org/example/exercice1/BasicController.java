b4de99989328bb55f3f864ace7f7a51b8f07ed23
JOSPINRemi <remidequarouble@gmail.com>
2025-01-20 15:02:49 +0100

    exercice1

---
+++ webflux/exercices/exercice1/src/main/java/org/example/exercice1/BasicController.java
@@ -0,0 +1,21 @@
+package org.example.exercice1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api")
+public class BasicController {
+    @GetMapping("/welcome")
+    public Mono<String> getMessage() {
+        return Mono.just("Welcome to Project Reactor!");
+    }
+
+    @GetMapping("/numbers")
+    public Flux<Integer> getNumbers() {
+        return Flux.range(1, 5);
+    }
+}
