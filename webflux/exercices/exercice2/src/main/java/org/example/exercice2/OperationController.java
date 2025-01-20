347a2887b1f1e1103a36b7aaf55b5c04ea2e096b
JOSPINRemi <remidequarouble@gmail.com>
2025-01-20 15:03:04 +0100

    exercice2

---
+++ webflux/exercices/exercice2/src/main/java/org/example/exercice2/OperationController.java
@@ -0,0 +1,18 @@
+package org.example.exercice2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/api")
+public class OperationController {
+    @GetMapping("/processed-numbers")
+    public Flux<String> getProcessedNumbers() {
+        return Flux.range(1,10)
+                .filter(number -> number % 2 == 0)
+                .map(number -> number * 10)
+                .map(number -> "Processed: " + number + "\t");
+    }
+}
