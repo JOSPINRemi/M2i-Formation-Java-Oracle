d645c64ae64347ca7bfed3caf605536227bafac6
JOSPINRemi <remidequarouble@gmail.com>
2025-01-22 12:18:03 +0100

    demo secu basic webflux

---
+++ webflux/demos/demo-secu-basic-webflux/src/main/java/org/example/demosecubasicwebflux/DemoSecuBasicWebfluxApplication.java
@@ -0,0 +1,13 @@
+package org.example.demosecubasicwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecuBasicWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSecuBasicWebfluxApplication.class, args);
+    }
+
+}
---
+++ webflux/demos/demo-secu-basic-webflux/src/test/java/org/example/demosecubasicwebflux/DemoSecuBasicWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demosecubasicwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecuBasicWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
