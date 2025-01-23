35d693bd0a97310918d491a7e27c73836a256711
JOSPINRemi <remidequarouble@gmail.com>
2025-01-23 11:27:26 +0100

    demo test opti webflux

---
+++ webflux/demos/demo-test-opti-webflux/src/main/java/org/example/demotestoptiwebflux/DemoTestOptiWebfluxApplication.java
@@ -0,0 +1,13 @@
+package org.example.demotestoptiwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTestOptiWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTestOptiWebfluxApplication.class, args);
+    }
+
+}
